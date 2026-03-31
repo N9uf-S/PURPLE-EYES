83b5f9b48132d534bae8eeba58991860b3e45257
9_s <joelyeboua6@gmail.com>
2026-03-31 21:31:46 +0000

    Create messageStyle.js

---
+++ utils/messageStyle.js
@@ -0,0 +1,18 @@
+import fs from "fs"
+import stylizedChar from "./fancy.js"
+
+export default function stylizedCardMessage(text) {
+  return {
+    text: stylizedChar(text),
+    contextInfo: {
+      externalAdReply: {
+        title: "קยгקɭє єץєร",
+        body: "℘ųཞ℘Ɩɛ ɛყɛʂ",
+        thumbnail: fs.readFileSync("./database/DigiX.jpg"),
+        sourceUrl: "https://whatsapp.com",
+        mediaType: 1,
+        renderLargerThumbnail: false
+      }
+    }
+  }
+}
